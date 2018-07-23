824d8ab4a48953e2de95442a29eba151cb69d1b5
wuguojun <wugj@chinatelecom.cn>
2018-07-23 17:54:14 +0800

    fix copy

--- src/main/java/cn/chinatelecom/dp/DockerInvoker.java
+++ src/main/java/cn/chinatelecom/dp/DockerInvoker.java
@@ -2,34 +2,56 @@ package cn.chinatelecom.dp;
 
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.ExecCreateCmdResponse;
-import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.command.*;
 import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.command.EventsResultCallback;
 import com.github.dockerjava.core.command.ExecStartResultCallback;
 import com.github.dockerjava.core.command.PullImageResultCallback;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
+
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class DockerInvoker {
 
 
-
     String fromImageRes = "hub.chinatelecom.cn/public/mlp:0.1";
-    String localFile = "/home/wu/Downloads/model2.py";
+    String localPythonFile = "/home/wu/Downloads/model2.py";
+
+    String trainFile = "/home/wu/Downloads/train.csv";
+    String testFile = "/home/wu/Downloads/test.csv";
+
+    String cmd = "/usr/bin/python /tmp/model2.py";
+
+    String destFile = "/home/wu/Downloads/sub.csv";
 
 
 
+
+    public static void unTar(TarArchiveInputStream tis, File destFile)
+            throws IOException {
+        TarArchiveEntry tarEntry = null;
+        while ((tarEntry = tis.getNextTarEntry()) != null) {
+            if (tarEntry.isDirectory()) {
+                if (!destFile.exists()) {
+                    destFile.mkdirs();
+                }
+            } else {
+                FileOutputStream fos = new FileOutputStream(destFile);
+                IOUtils.copy(tis, fos);
+                fos.close();
+            }
+        }
+        tis.close();
+    }
 
     public String getContainerIdUsingName(String containerName) {
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
@@ -39,12 +61,12 @@ public class DockerInvoker {
         return containerInfo.getId();
     }
 
-        public void invoke( ) throws InterruptedException,FileNotFoundException {
+    public void invoke() throws InterruptedException, FileNotFoundException,IOException {
 
 
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
 
-            DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
 
 //        PullImageCmd pullImageCmd = dockerClient.pullImageCmd(fromImageRes);
 //        PullImageResultCallback callback = new PullImageResultCallback() {
@@ -61,34 +83,65 @@ public class DockerInvoker {
 //        pullImageCmd.exec(callback).awaitSuccess();
 
 
-          File file = new File(localFile);
-            InputStream input = new FileInputStream(file);
-            TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(input,"UTF-8");
 
-          CreateContainerResponse container = dockerClient.createContainerCmd(fromImageRes).exec();
-            dockerClient.copyArchiveToContainerCmd(getContainerIdUsingName("distracted_nobel")).withRemotePath("/tmp").withHostResource(localFile).withNoOverwriteDirNonDir(false).exec();
-            System.out.println("Copying");
-
-       // dockerClient.startContainerCmd(container.getId()).exec();
-
-//        dockerClient.copyArchiveToContainerCmd(container.getId()).withRemotePath("/tmp").withTarInputStream(tarArchiveInputStream) .exec();
-//        System.out.println("Copying");
+        EventsResultCallback callback = new EventsResultCallback() {
+            @Override
+            public void onNext(Event event) {
+                System.out.println("Event: " + event);
+                super.onNext(event);
+            }
+        };
 
 
+        String containerId =  getContainerIdUsingName("lucid_morse");
+        dockerClient.copyArchiveToContainerCmd(containerId).withRemotePath("/tmp").withHostResource(trainFile).withNoOverwriteDirNonDir(false).exec();
+        System.out.println("Copying");
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        ByteArrayOutputStream stderr = new ByteArrayOutputStream();
 
 
-//        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
-//        ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+        ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(containerId)
+                .withAttachStdout(true)
+               .withAttachStderr(true)
+                .withAttachStdin(true)
+                .withCmd("/usr/bin/python", "/tmp/model2.py").exec();
+
+        System.out.println(execCreateCmdResponse);
+
+        dockerClient.execStartCmd(execCreateCmdResponse.getId())
+                            .withDetach(false).
+        exec(new ExecStartResultCallback(stdout, stderr)).awaitCompletion();
+
+        System.out.println(stdout);
+        System.out.println("-----------------");
+        System.out.println(stderr);
 
 
-//        CreateContainerResponse container = dockerClient.copyArchiveFromContainerCmd(containerResp.getId(),"/home/wu/hello.txt")
-//                .withHostPath("/tmp").exec();
-//        dockerClient.startContainerCmd(container.getId()).exec();
+        // Copy file from container
+        TarArchiveInputStream tarStream = new TarArchiveInputStream(
+                dockerClient.copyArchiveFromContainerCmd(containerId, "/tmp/temp/submission_1532335035.csv").exec());
+        try {
+            unTar(tarStream, new File(destFile));
+        } finally {
+            tarStream.close();
+        }
 
 
-//        String containerId = containerResp.getId();
+//        // Copy file from container
+//        InputStream inputStream = dockerClient
+//                .copyArchiveFromContainerCmd(containerId, "/tmp/temp/submission_1532335035.csv")
+//                .withHostPath("/tmp")
+//                .exec();
+//        inputStream.available();
+//        copyInputStreamToFile(inputStream, new File("/home/wu/Downloads/submission_1532335035.csv"));
+
+
+
 //        dockerClient.startContainerCmd(containerId).exec();
 //
+//        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+//        ByteArrayOutputStream stderr = new ByteArrayOutputStream();
 //
 //        ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(containerId)
 //                .withAttachStdout(true)
@@ -97,14 +150,14 @@ public class DockerInvoker {
 //                .exec();
 //        dockerClient.execStartCmd(execCreateCmdResponse.getId()).exec(
 //                new ExecStartResultCallback(stdout, stderr)).awaitCompletion();
-
-
-//        CreateContainerResponse container = dockerClient.createContainerCmd("hub.chinatelecom.cn/public/mlp:0.1")
-//                .withImage("hub.chinatelecom.cn/public/mlp")
-//                .withCmd("sleep", "9999")
-//                .withName("container1")
-//                .exec();
 //
+
+
+
+//        CreateContainerResponse container = dockerClient.copyArchiveFromContainerCmd(containerResp.getId(),"/home/wu/hello.txt")
+//                .withHostPath("/tmp").exec();
+//        dockerClient.startContainerCmd(container.getId()).exec();
+
 //        dockerClient.startContainerCmd(container.getId()).exec();
 //
 //        ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(container.getId())
@@ -116,29 +169,9 @@ public class DockerInvoker {
 //                new ExecStartResultCallback(stdout, stderr)).awaitCompletion();
 
 
-//        DockerClient client = DockerClientBuilder.getInstance(config).build();
-//        CreateContainerResponse containerResp = client.createContainerCmd("busybox")
-//                .withImage("192.168.4.12:5000/my-ubuntu")
-//                .withCmd("sleep", "9999")
-//                .withAttachStderr(true)
-//                .withAttachStdout(true)
-//                .exec();
-//        String containerId = containerResp.getId();
-//        client.startContainerCmd(containerId).exec();
-//
-//        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
-//        ByteArrayOutputStream stderr = new ByteArrayOutputStream();
-//
-//        ExecCreateCmdResponse execCreateCmdResponse = client.execCreateCmd(containerId)
-//                .withAttachStdout(true)
-//                .withAttachStderr(true)
-//                .withCmd("ping", "8.8.8.8", "-c", "30")
-//                .exec();
-//        client.execStartCmd(execCreateCmdResponse.getId()).exec(
-//                new ExecStartResultCallback(stdout, stderr)).awaitCompletion();
     }
 
-    public static  void main(String []args) throws InterruptedException,FileNotFoundException{
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException,IOException {
 
         DockerInvoker dockerInvoker = new DockerInvoker();
         dockerInvoker.invoke();
